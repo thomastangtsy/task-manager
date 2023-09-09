0094131653ad3d55ad27b02682e19c0cbed30c89
Sikyin Tang <thomas2d19@gmail.com>
2023-09-08 23:59:35 -0400

    Close modal after adding a new task

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import Modal from "./components/Modal";
 import TaskForm from "./components/TaskForm";
 import useTasks from "./hooks/useTasks";
 import Button from "./components/Button";
+import { InitialTask } from "./models/task";
 
 const App: FC = () => {
   const { tasks, addTask, removeTask } = useTasks();
 
   const [modalOpen, setModalOpen] = useState(false);
+
+  const addTaskAndCloseModal = (task: InitialTask) => {
+    addTask(task);
+    setModalOpen(false);
+  };
 
   return (
     <div className="container mx-auto my-4 px-2">
@@ -21,7 +27,7 @@ const App: FC = () => {
         <Modal isOpen={modalOpen} close={() => setModalOpen(false)}>
           <div className="min-w-[80vw]">
             <h1 className="text-3xl font-bold mb-4">Add a new task</h1>
-            <TaskForm addTask={addTask}></TaskForm>
+            <TaskForm addTask={addTaskAndCloseModal}></TaskForm>
           </div>
         </Modal>
       </header>
